66da1b5b36281ecf5a877855ec22e1d75a7b9097
alexjung417 <125166704+alexjung417@users.noreply.github.com>
2023-10-18 12:51:05 -0700

    barrels should be fixed now

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -66,10 +66,10 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).first().gold 
         potions = connection.execute(sqlalchemy.text("SELECT * FROM potions"))
         a = []
-        for Barrel in wholesale_catalog:
-            for potion in potions:
-                ptype = [x//100 for x in list(potion.type)]
-                if potion.quantity < 10 and gold >= Barrel.price and Barrel.potion_type == ptype:
+        for potion in potions:
+            for Barrel in wholesale_catalog:
+                ptype = [1 if x != 0 else 0 for x in potion.type]
+                if (potion.quantity < 10) and (gold >= Barrel.price) and (Barrel.potion_type == ptype):
                     a.append({
                             "sku": Barrel.sku,
                             "ml_per_barrel": Barrel.ml_per_barrel,
@@ -78,9 +78,27 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                             "quantity": 1
                             })
                     gold -= Barrel.price
+                print( potion.quantity, gold, ptype)
 
     return a
-# [Barrel(sku='LARGE_RED_BARREL', ml_per_barrel=10000, potion_type=[1, 0, 0, 0], price=500, quantity=30), 
-# Barrel(sku='MEDIUM_RED_BARREL', ml_per_barrel=2500, potion_type=[1, 0, 0, 0], price=250, quantity=10), 
-# Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=10), 
-# Barrel(sku='MINI_RED_BARREL', ml_per_barrel=200, potion_type=[1, 0, 0, 0], price=60, quantity=1)]
+# [{"sku": "MEDIUM_RED_BARREL", "ml_per_barrel": 2500, "potion_type": [1, 0, 0, 0], "price": 250, "quantity": 10
+#   },{"sku": "SMALL_RED_BARREL", "ml_per_barrel": 500, "potion_type": [1, 0, 0, 0], "price": 100, "quantity": 10
+#   },{
+#     "sku": "MEDIUM_GREEN_BARREL", "ml_per_barrel": 2500, "potion_type": [0, 1, 0, 0], "price": 250, "quantity": 10
+#   },{
+#     "sku": "SMALL_GREEN_BARREL", "ml_per_barrel": 500, "potion_type": [0, 1, 0, 0], "price": 100, "quantity": 10
+#   },{
+#     "sku": "MEDIUM_BLUE_BARREL", "ml_per_barrel": 2500, "potion_type": [0, 0, 1, 0], "price": 300, "quantity": 10
+#   },
+#   {
+#     "sku": "SMALL_BLUE_BARREL", "ml_per_barrel": 500, "potion_type": [0, 0, 1, 0], "price": 120, "quantity": 10
+#   },
+#   {
+#     "sku": "MINI_RED_BARREL", "ml_per_barrel": 200, "potion_type": [1, 0, 0, 0], "price": 60, "quantity": 1
+#   },
+#   {
+#     "sku": "MINI_GREEN_BARREL", "ml_per_barrel": 200, "potion_type": [0, 1, 0, 0], "price": 60, "quantity": 1
+#   },
+#   {
+#     "sku": "MINI_BLUE_BARREL", "ml_per_barrel": 200, "potion_type": [0, 0, 1, 0], "price": 60, "quantity": 1
+#   }]
